57d4317e34205cf0f0c02d12b3e6bde73f733ed6
jlvihv <imvihv@gmail.com>
2024-11-19 14:11:28 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  base: "/ollama-kiss-ui/",
+export default defineConfig(({ command, mode }) => {
+  // 判断是否是 GitHub Pages 环境
+  const isGitHubPages = mode === "github-pages";
+
+  return {
+    plugins: [svelte()],
+    // 条件设置 base
+    base: isGitHubPages ? "/ollama-kiss-ui/" : "/",
+  };
 });
